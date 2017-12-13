70248c7ca6ff34ea1ea8597dec62ceb518787a94
DenysiukVitalii <dendoc97@gmail.com>
2017-12-14 01:18:18 +0200

    update sql-script

--- schedule.sql
+++ schedule.sql
@@ -65,11 +65,11 @@ CREATE TABLE Teachers (
 
 CREATE TABLE TypeLesson (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
+    type_lesson VARCHAR(50) NOT NULL
 );
 
 
-CREATE TABLE Сurriculum (
+CREATE TABLE Сurriculums (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     teacherID INT NOT NULL,
     subjectID INT NOT NULL,
@@ -88,7 +88,7 @@ CREATE TABLE AmountHours (
     type_lessonID INT NOT NULL,
     curriculumID INT NOT NULL,
     FOREIGN KEY (type_lessonID) REFERENCES TypeLesson(id),
-    FOREIGN KEY (curriculumID) REFERENCES Сurriculum(id),
+    FOREIGN KEY (curriculumID) REFERENCES Сurriculums(id),
     UNIQUE(id) 
 );
 
@@ -112,15 +112,15 @@ CREATE TABLE Schedules (
     subjectID INT NOT NULL,
     type_lessonID INT NOT NULL,
     semesterID INT NOT NULL,
-    specialtyID INT NOT NULL,
+    audienceID INT NOT NULL,
     
-    FOREIGN KEY (teacherID) REFERENCES Teacher(id),
+    FOREIGN KEY (teacherID) REFERENCES Teachers(id),
     FOREIGN KEY (subjectID) REFERENCES Subjects(id),
-    FOREIGN KEY (type_lessonID) REFERENCES TypeLesson(id),
-    FOREIGN KEY (semesterID) REFERENCES Semester(number_semester),
-    FOREIGN KEY (specialtyID) REFERENCES Specialty(id),
-    FOREIGN KEY (groupID) REFERENCES Un_group(id),
     FOREIGN KEY (dayID) REFERENCES Date_day(id),
+    FOREIGN KEY (groupID) REFERENCES Un_group(id),
+    FOREIGN KEY (type_lessonID) REFERENCES TypeLesson(id),
+    FOREIGN KEY (semesterID) REFERENCES Semesters(number_semester),
+    FOREIGN KEY (audienceID) REFERENCES Audience(id),
     
 	UNIQUE(id) 
 );
@@ -132,7 +132,7 @@ INSERT INTO `Specialty` (`spec_name`) VALUES
 ('Software Engineering'),
 ('Computer Science'),
 ('Hardware Engineering');
-
+select * from Specialty;
 UPDATE Specialty SET spec_name = 'testaa' WHERE id = 4;
 
 
@@ -145,6 +145,10 @@ INSERT INTO `Date_day` (`day`) VALUES
 ('Saturday');
 
 INSERT INTO `Un_group` (`id`, `specialtyID`, `course`, `amount_students`) VALUES
+('TP-21', 1, 6, 20),
+('TR-31', 2, 5, 22),
+('TE-21', 1, 6, 20),
+('TK-31', 2, 5, 22),
 ('TV-51', 1, 3, 20),
 ('TR-61', 2, 2, 22),
 ('TO-31', 3, 4, 17),
@@ -152,9 +156,26 @@ INSERT INTO `Un_group` (`id`, `specialtyID`, `course`, `amount_students`) VALUES
 ('KT-61', 2, 2, 21),
 ('BN-31', 3, 4, 20);
 truncate Un_group;
+select * from Audience;
 
-select * from Semesters;
-truncate Semesters;
+SELECT Un_group.id, course, amount_students FROM Un_group, Specialty
+WHERE specialtyID=Specialty.id AND specialtyID=2;
+
+CREATE OR REPLACE VIEW group_view AS
+SELECT Un_group.id, spec_name, course, amount_students FROM Un_group, Specialty
+WHERE specialtyID=Specialty.id AND course > 4 WITH CHECK OPTION;
+
+SELECT * FROM group_view WHERE amount_students > 21;
+
+UPDATE group_view SET amount_students=19 WHERE id='TP-21';
+UPDATE group_view SET course=6 WHERE id='TP-21';
+
+DROP VIEW group_view;
+
+select * from Date_day;
+delete from Date_day;
+truncate Date_day;
+
 INSERT INTO `Semesters` (`number_semester`, `init_data`, `end_data`) VALUES
 (1, '2017-09-1', '2017-12-30'),
 (2, '2018-02-10', '2018-06-12'),
@@ -202,14 +223,47 @@ INSERT INTO `Teachers` (`surname`, `name`, `lastname`, `position`, `rank`, `phon
 ALTER TABLE Specialty CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE Specialty DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
+start transaction;
+INSERT INTO `Teachers` (`surname`, `name`, `lastname`, `position`, `rank`, `phone`) VALUES
+('Pupkin', 'Vasya', 'Yurievich', 'Software developer', 'Docent', '0678926323');
+select * from Teachers;
+rollback;
+select * from Teachers;
+
+delimiter $$ 
+drop procedure if exists teacher_cursor $$
+create procedure teacher_cursor()
+begin 
+	declare v_surname varchar(50);
+    declare v_name varchar(50);
+    declare v_phone varchar(20);
+    declare done integer default 0;
+    declare c1 cursor for select surname, name, phone from Teachers;
+    declare continue handler for NOT FOUND set done = 1;
+    open c1;
+		get_info: LOOP
+			fetch c1 into v_surname, v_name, v_phone;
+			if done = 1 then 
+				leave get_info;
+			end if;
+			select concat(v_surname, ' ', v_name) as Teacher, v_phone as Phone;
+		END LOOP get_info;
+    close c1;
+end $$
+
+delimiter ;
+SET SQL_SAFE_UPDATES = 1;
+call teacher_cursor();
+
 select * from TypeLesson;
 
-INSERT INTO `TypeLesson` (`name`) VALUES
+
+INSERT INTO `TypeLesson` (type_lesson) VALUES
 ('Lecture'),
 ('Practice'),
 ('Laboratory');
 
-INSERT INTO `Сurriculum` (`specialtyID`, `semesterID`, `subjectID`, `teacherID`) VALUES
+INSERT INTO Сurriculums (specialtyID, semesterID, subjectID, teacherID) VALUES
 (1,1,6,1),
 (1,1,8,1),
 (1,1,4,2),
@@ -219,6 +273,9 @@ INSERT INTO `Сurriculum` (`specialtyID`, `semesterID`, `subjectID`, `teacherID`
 (2,1,4,2),
 (2,1,5,2);
 
+SELECT * FROM Сurriculums;
+delete from Сurriculums where id = 6;
+
 INSERT INTO AmountHours (curriculumID, type_lessonID, amount_hours) VALUES
 (1, 1, 36),
 (1, 2, 24),
@@ -227,15 +284,16 @@ INSERT INTO AmountHours (curriculumID, type_lessonID, amount_hours) VALUES
 (3, 1, 36),
 (3, 2, 24),
 (4, 1, 36),
-(4, 2, 24),
-(5, 1, 36),
-(5, 2, 24),
-(6, 1, 36),
-(6, 2, 24);
+(4, 2, 24);
 
-select * from Сurriculum;
-truncate Сurriculum;
-DELETE FROM Сurriculum;
+
+UPDATE AmountHours SET amount_hours = 24    
+WHERE curriculumID = 8 and type_lessonID = 1;
+INSERT INTO AmountHours (curriculumID, type_lessonID, amount_hours) VALUES
+(8, 1, 36);
+select * from AmountHours;
+truncate Сurriculums;
+DELETE FROM Сurriculums;
 
 INSERT INTO `Audience` (`number_audience`, `building`, `amount_seats`) VALUES
 (314, 5, 50),
@@ -246,7 +304,7 @@ INSERT INTO `Audience` (`number_audience`, `building`, `amount_seats`) VALUES
 
 INSERT INTO `Schedules` (`number_lesson`, `number_week`, `dayID`, `groupID`,
 						 `teacherID`, `subjectID`, `type_lessonID`, `semesterID`,
-                         `specialtyID`) VALUES
+                         `audienceID`) VALUES
 (1,1,1,'TV-51', 1, 1, 1, 1, 1),
 (2,1,1,'TV-51', 1, 1, 2, 1, 1),
 (3,1,1,'TV-51', 2, 3, 1, 1, 1),
@@ -266,48 +324,142 @@ FROM Un_group
 LEFT JOIN Specialty ON Un_group.specialtyID = Specialty.id
 ORDER BY Un_group.course;
 
-select Specialty.spec_name, 
-	   Un_group.id as 'group',
+select Schedules.id,
+	   concat(Audience.number_audience, '-', Audience.building) as 'place',
        number_lesson, 
-       number_week, 
        Date_day.day, 
-       TypeLesson.name as 'type_lesson',
+       TypeLesson.type_lesson as 'type_lesson',
        Subjects.subject_name, 
-       concat(Teacher.surname, ' ', left(Teacher.name, 1), '.', left(Teacher.lastname, 1), '.') as 'teacher',  
-       Semester.number_semester as 'semester' from Schedules
-join Specialty on Schedules.specialtyID = Specialty.id
+       concat(Teachers.surname, ' ', left(Teachers.name, 1), '.', left(Teachers.lastname, 1), '.') as 'teacher'
+       from Schedules
+join Audience on Schedules.audienceID = Audience.id
 join Un_group on Schedules.groupID = Un_group.id
 join Subjects on Schedules.subjectID = Subjects.id
 join Date_day on Schedules.dayID = Date_day.id
 join TypeLesson on Schedules.type_lessonID = TypeLesson.id
-join Teacher on Schedules.teacherID = Teacher.id
-join Semester on Schedules.semesterID = Semester.number_semester;
+join Teachers on Schedules.teacherID = Teachers.id
+join Semesters on Schedules.semesterID = Semesters.number_semester
+where Schedules.groupID = 'TV-51'
+and Schedules.semesterID = 1;
 
-select Сurriculum.id,
-	   Specialty.spec_name,
-       Semesters.number_semester,
+CREATE OR REPLACE VIEW getScheduleByGroup AS
+select Schedules.id,
+	   concat(Audience.number_audience, '-', Audience.building) as 'place',
+       number_lesson, 
+       Date_day.day,
+       Schedules.groupID,
+       Schedules.semesterID,
+       Schedules.number_week,
+       TypeLesson.type_lesson as 'type_lesson',
        Subjects.subject_name, 
-       concat(Teachers.surname, ' ', left(Teachers.name, 1), '.', left(Teachers.lastname, 1), '.') as 'teacher'  
-       from Сurriculum
-join Specialty on Сurriculum.specialtyID = Specialty.id
-join Semesters on Сurriculum.semesterID = Semesters.number_semester
-join Subjects on Сurriculum.subjectID = Subjects.id
-join Teachers on Сurriculum.teacherID = Teachers.id;
+       concat(Teachers.surname, ' ', left(Teachers.name, 1), '.', left(Teachers.lastname, 1), '.') as 'teacher'
+       from Schedules
+join Audience on Schedules.audienceID = Audience.id
+join Un_group on Schedules.groupID = Un_group.id
+join Subjects on Schedules.subjectID = Subjects.id
+join Date_day on Schedules.dayID = Date_day.id
+join TypeLesson on Schedules.type_lessonID = TypeLesson.id
+join Teachers on Schedules.teacherID = Teachers.id
+join Semesters on Schedules.semesterID = Semesters.number_semester
+WITH CHECK OPTION;
+
+SELECT * FROM getScheduleByGroup 
+where groupID = 'TV-51'
+and number_week = 1;
+
+CREATE OR REPLACE VIEW checkAvailableTeacher AS
+
+select Schedules.id,
+	   Schedules.dayID,
+       Schedules.teacherID, 
+       Schedules.number_lesson, 
+       Schedules.number_week
+	   from Schedules
+join Date_day on Schedules.dayID = Date_day.id
+join Teachers on Schedules.teacherID = Teachers.id
+
+WITH CHECK OPTION;
+
+SELECT * FROM checkAvailableTeacher 
+where dayID = 1 
+and teacherID = 1 
+and number_lesson = 1 
+and number_week = 1;
+
+
+CREATE OR REPLACE VIEW checkAvailableAudience AS
+
+select Schedules.id,
+	   Schedules.dayID,
+       Schedules.audienceID, 
+       Schedules.number_lesson, 
+       Schedules.number_week
+	   from Schedules
+join Audience on Schedules.audienceID = Audience.id
+join Date_day on Schedules.dayID = Date_day.id
+join Teachers on Schedules.teacherID = Teachers.id
+
+WITH CHECK OPTION;
+
+SELECT * FROM checkAvailableAudience 
+where dayID = 1 
+and audienceID = 1 
+and number_lesson = 1 
+and number_week = 1;
+
+
+CREATE OR REPLACE VIEW getCurriculumBySpec AS
+select Сurriculums.id,
+       Сurriculums.semesterID,
+       Сurriculums.specialtyID,
+       Сurriculums.subjectID,
+       Сurriculums.teacherID,
+       Subjects.subject_name, 
+       concat(Teachers.surname, ' ',
+			  left(Teachers.name, 1), '.', 
+			 left(Teachers.lastname, 1), '. (', Teachers.position, ')') as 'teacher'  
+       from Сurriculums
+join Specialty on Сurriculums.specialtyID = Specialty.id
+join Semesters on Сurriculums.semesterID = Semesters.number_semester
+join Subjects on Сurriculums.subjectID = Subjects.id
+join Teachers on Сurriculums.teacherID = Teachers.id WITH CHECK OPTION;
+
+SELECT * FROM getCurriculumBySpec WHERE 
+semesterID = 1 and specialtyID = 1;
+
+CREATE OR REPLACE VIEW getTypesLessonByCurriculum AS
+select AmountHours.curriculumID,
+		TypeLesson.type_lesson,
+       TypeLesson.id,
+       AmountHours.amount_hours
+       from AmountHours
+join Сurriculums on Сurriculums.id = AmountHours.curriculumID
+join Specialty on Specialty.id = Сurriculums.specialtyID
+join Semesters on Semesters.number_semester = Сurriculums.semesterID
+join Subjects on Subjects.id = Сurriculums.subjectID
+join Teachers on Teachers.id = Сurriculums.teacherID
+join TypeLesson on TypeLesson.id = AmountHours.type_lessonID
+WITH CHECK OPTION;
+delete from AmountHours where curriculumID = 9;
+SELECT * FROM getTypesLessonByCurriculum WHERE curriculumID = 1;
+
 
 select AmountHours.curriculumID,
 	   Specialty.spec_name,
        Semesters.number_semester,
        Subjects.subject_name, 
        concat(Teachers.surname, ' ', left(Teachers.name, 1), '.', left(Teachers.lastname, 1), '.') as 'teacher', 
-       TypeLesson.name,
+       TypeLesson.type_lesson,
+       TypeLesson.id,
        AmountHours.amount_hours
        from AmountHours
-join Сurriculum on Сurriculum.id = AmountHours.curriculumID
-join Specialty on Specialty.id = Сurriculum.specialtyID
-join Semesters on Semesters.number_semester = Сurriculum.semesterID
-join Subjects on Subjects.id = Сurriculum.subjectID
-join Teachers on Teachers.id = Сurriculum.teacherID
-join TypeLesson on TypeLesson.id = AmountHours.type_lessonID;
+join Сurriculums on Сurriculums.id = AmountHours.curriculumID
+join Specialty on Specialty.id = Сurriculums.specialtyID
+join Semesters on Semesters.number_semester = Сurriculums.semesterID
+join Subjects on Subjects.id = Сurriculums.subjectID
+join Teachers on Teachers.id = Сurriculums.teacherID
+join TypeLesson on TypeLesson.id = AmountHours.type_lessonID
+where AmountHours.curriculumID = 1;
 
 select GROUP_CONCAT('\`', 
 	json_object('type_lesson',TypeLesson.name, 
@@ -319,7 +471,7 @@ join Сurriculum on Сurriculum.id = AmountHours.curriculumID
 where Сurriculum.id = AmountHours.curriculumID;
 
 select json_object(
-  'id',  Сurriculum.id,
+  'id',  Сurriculums.id,
   'subject_id', Subjects.id,
   'teacher_id', Teachers.id,
   'subject', Subjects.subject_name,
@@ -327,15 +479,17 @@ select json_object(
   'types_lesson', json_array(
 		(select GROUP_CONCAT('\`', 
 								json_object('type_lesson',TypeLesson.name, 
-											'amount_hours', AmountHours.amount_hours), '\`'
+											'amount_hours', AmountHours.amount_hours,
+                                            'selected', true,
+                                            'id', AmountHours.curriculumID), '\`'
 		)
 		from AmountHours
 		join TypeLesson on TypeLesson.id = AmountHours.type_lessonID
-		where Сurriculum.id = AmountHours.curriculumID))) as curriculum
-from Сurriculum
-join Subjects on Subjects.id = Сurriculum.subjectID
-join Teachers on Teachers.id = Сurriculum.teacherID
-WHERE Сurriculum.semesterID = 1 and Сurriculum.specialtyID = 2;
+		where Сurriculums.id = AmountHours.curriculumID))) as curriculum
+from Сurriculums
+join Subjects on Subjects.id = Сurriculums.subjectID
+join Teachers on Teachers.id = Сurriculums.teacherID
+WHERE Сurriculums.semesterID = 1 and Сurriculums.specialtyID = 1;
 
 
 select spec_name from Specialty
